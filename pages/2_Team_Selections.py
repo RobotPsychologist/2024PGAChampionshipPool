b15ff7f4b5eab3d76df905230cef09468e53ffdc
RobotPsychologist <110344005+RobotPsychologist@users.noreply.github.com>
2024-05-16 14:52:50 -0400

    deleted whitespace.

--- pages/2_Team_Selections.py
+++ pages/2_Team_Selections.py
@@ -62,5 +62,3 @@ with group2:
         st.write(f"### Group {i}")
         st.dataframe(df[df['group_number'] == i][['golfer', 'pick_count','THRU','SCORE', 'TODAY','R1','R2','R3','R4','TOT']], hide_index=True, use_container_width=True, column_config=COLUMN_CONFIG)
 
-
-
