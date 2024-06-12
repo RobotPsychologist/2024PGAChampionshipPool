73ad2d656922750ec59a6367c906d264e62b8280
RobotPsychologist <cjr.robot.psychologist@gmail.com>
2024-06-12 12:06:06 -0400

    Error handling.

--- pages/4_Pool_Scorecards.py
+++ pages/4_Pool_Scorecards.py
@@ -40,23 +40,26 @@ def team_selection_table(team, pool_team_selections_df, pool_player_names_df=pla
     '''Displays the team selection table with populated scores for a given team'''
     pool_player_name = pool_player_names_df[team]
     st.write(f"### {pool_player_name}")
-    col_select = ['group_number','POS', 'golfer','SCORE','TODAY','THRU','TOT']
-    st.dataframe(pool_team_selections_df.loc[pool_team_selections_df['player'] == pool_player_name,
-                                             col_select],
-                 hide_index=True,
-                 use_container_width=True,
-                 column_config={
-                        "group_number": st.column_config.NumberColumn("G#"),
-                        "golfer": st.column_config.TextColumn("GOLFER", width="medium"),
-                        "TOT": st.column_config.NumberColumn(
-                            "Total",
-                            width="small",
-                            min_value=-100,
-                            max_value=1500,
-                            step=1,
-                        )
-                    }
-                )
+    try:
+        col_select = ['group_number','POS', 'golfer','SCORE','TODAY','THRU','TOT']
+        st.dataframe(pool_team_selections_df.loc[pool_team_selections_df['player'] == pool_player_name,
+                                                col_select],
+                    hide_index=True,
+                    use_container_width=True,
+                    column_config={
+                            "group_number": st.column_config.NumberColumn("G#"),
+                            "golfer": st.column_config.TextColumn("GOLFER", width="medium"),
+                            "TOT": st.column_config.NumberColumn(
+                                "Total",
+                                width="small",
+                                min_value=-100,
+                                max_value=1500,
+                                step=1,
+                            )
+                        }
+                    )
+    except:
+        st.write("Wating for tournament to start or data loading erorr...")
  
 df_golfers = pull_scores()
 score_cards = pd.merge(df_player_picks, df_golfers, how='left', left_on='golfer', right_on='PLAYER') 
